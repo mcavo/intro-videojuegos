246ba1d00e369c20b32fd2546d0c82ca2adcb98c
mariaeugenia <me.sakuda@hotmail.com>
2016-08-20 18:10:10 -0300

    Adding first car moving with constant velocity

---
+++ tp1/freeway/Assets/Scripts/littleCarPink.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class littleCarPink : MonoBehaviour {
+
+	public float speed = 1f;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		Debug.Assert(rb != null, "Cars needs to have a RigidBody when initialize");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = transform.position;
+		transform.position = new Vector3(currentPosition.x - speed, currentPosition.y, currentPosition.z);
+		checkBoundaries();
+	}
+
+	private void checkBoundaries() {
+		//x = 0 is the mid of the screen
+		// Por qué tengo que poner ese 100 inmundo?? 
+		if (transform.position.x < Screen.width / -100f) { 
+			transform.position = new Vector3 (Screen.width/100, transform.position.y, transform.position.z);
+		}
+	}
+}
