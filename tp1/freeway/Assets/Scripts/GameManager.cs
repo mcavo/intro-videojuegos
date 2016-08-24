d7e587ba265bf760b778eb610264adebc8cb6cd4
mariaeugenia <me.sakuda@hotmail.com>
2016-08-23 21:12:29 -0300

    Adding timer and fixing by code score text

--- tp1/freeway/Assets/Scripts/GameManager.cs
+++ tp1/freeway/Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Timers;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
 	public static GameManager instance = null;				//Static instance of GameManager which allows it to be accessed by any other script.
 	private BoardManager boardScript;						//Store a reference to our BoardManager which will set up the level.
+	public float timeLeft = 25.0F;
 
 	// Use this for initialization
 	void Awake () {
@@ -30,11 +33,29 @@ public class GameManager : MonoBehaviour {
 		//Call the InitGame function to initialize the first level 
 		InitGame();
 	}
-	
+
+	void Update() {
+		double oldTime = System.Math.Round(timeLeft,2);
+		timeLeft -= Time.deltaTime / 4;
+		double newTime = System.Math.Round(timeLeft,2);
+		if (oldTime - newTime != 0) {
+			boardScript.timeText.text = string.Format ("{0}", newTime.ToString("F2"));
+
+		}	
+		validateGameOver ();
+	}
+
 	//Initializes the game for each level.
 	void InitGame() {
 		
 		//Call the SetupScene function of the BoardManager script, pass it current difficulty number.
 		boardScript.SetupScene(0); //TODO: change difficulty from menu.
+	}
+
+	private void validateGameOver() {
+		if (timeLeft < 0) {
+			Debug.Log ("Game Over");
+			// Here we should show the initial menu
+		}
 	}
 }
