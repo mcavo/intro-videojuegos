62512ba7464e442f7e3a0727c2eabf2ea2bae6dc
Victoria <mvictoria.cavo@gmail.com>
2016-09-15 15:42:00 -0300

    Start Pacmania

---
+++ tp2/Pacmania/Assets/Scripts/PointScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointScript : MonoBehaviour
+{
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag.Equals ("Pacman"))
+		{
+			Destroy(gameObject); 
+		}
+	}
+}
