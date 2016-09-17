8d3fd9e24f0db367a2ff444f5ad78b09db36f8ff
Victoria <mvictoria.cavo@gmail.com>
2016-09-17 14:06:53 -0300

    Add brackets to if statement

--- tp2/Pacmania/Assets/Scripts/GameLoader.cs
+++ tp2/Pacmania/Assets/Scripts/GameLoader.cs
@@ -10,13 +10,14 @@ public class GameLoader : MonoBehaviour
 	void Awake ()
 	{
 		//Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
-		if (GameManager.instance == null)
+		if (GameManager.instance == null) {
 			//Instantiate gameManager prefab
-			Instantiate(gameManager);
-
+			Instantiate (gameManager);
+		}
 		//Check if a SoundManager has already been assigned to static variable GameManager.instance or if it's still null
-		//if (SoundManager.instance == null)
+		//if (SoundManager.instance == null) {
 			//Instantiate SoundManager prefab
 			//Instantiate(soundManager);
+		//}
 	}
 }
