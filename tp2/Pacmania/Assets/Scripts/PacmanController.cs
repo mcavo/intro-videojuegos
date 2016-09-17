4f99e02f4ad76555343cdee5a3c1ffe6e6f2ce43
Victoria <mvictoria.cavo@gmail.com>
2016-09-17 03:03:30 -0300

    Add Pacman movement

--- tp2/Pacmania/Assets/Scripts/PacmanController.cs
+++ tp2/Pacmania/Assets/Scripts/PacmanController.cs
@@ -1,24 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class PacmanController : MonoBehaviour {
 
 	public float MovementSpeed = 0f;
 
 	private Animator animator;
 
+	private int movementOffset = 4;
+	private int[] matrixOffset = new int[2] {-37,41};
+
 	private Vector3 up = Vector3.zero,
-	down = new Vector3(0,180,0),
-	right = new Vector3(0,90,0),
-	left = new Vector3(0,270,0),
-	currentDirection = Vector3.zero;
+					down = new Vector3(0,180,0),
+					right = new Vector3(0,90,0),
+					left = new Vector3(0,270,0),
+					nextDirection = Vector3.zero,
+					currentDirection = Vector3.zero;
 	private Vector3 initialPosition = new Vector3(1,-7.5f,-7);
 
 	public void Reset() {
 		transform.position = initialPosition;
 		animator.SetBool ("IsDead", false);
 		animator.SetBool ("IsMoving", false);
-		currentDirection = right;
+		currentDirection = down;
+		nextDirection = down;
+	}
+
+	private bool AtCheckPoint() {
+		Vector3 pos = transform.position;
+		return Mathf.Approximately (0.25f, (float)(System.Math.Round (pos.x / 4f, 2) - Mathf.Floor (pos.x / 4f))) &&
+		Mathf.Approximately (0.25f, (float)(System.Math.Round (pos.z / 4f, 2) - Mathf.Floor (pos.z / 4f)));
+	}
+
+	private void UpdateDirection() {
+		if (currentDirection != nextDirection) {
+			transform.localEulerAngles = nextDirection;
+			if (CanMove (GetBoardPosition(RoundVector3(transform.position + transform.forward * movementOffset, 2)))) {
+				currentDirection = nextDirection;
+			}
+			transform.localEulerAngles = currentDirection;
+		}
+	}
+
+	private bool CanMove( int[] toPosition ) {
+		return GameManager.instance.board [toPosition[1], toPosition[0]] == 0;
+	}
+
+	private int[] GetBoardPosition( Vector3 position ) {
+		return new int[2] { (int) ((position.x - matrixOffset [0]) / movementOffset), (int) ((matrixOffset [1] - position.z) / movementOffset) };
 	}
 
 	void Start() {
@@ -31,26 +61,42 @@ public class PacmanController : MonoBehaviour {
 		var isMoving = true;
 		var isDead = animator.GetBool ("IsDead");
 
+		int[] h = GetBoardPosition (transform.position);
+
 		if (isDead) {
 			isMoving = false;
 		} else if (Input.GetKey(KeyCode.UpArrow)) {
-			currentDirection = up;
+			nextDirection = up;
+			Debug.Log ("up");
 		} else if (Input.GetKey(KeyCode.DownArrow)) {
-			currentDirection = down;
+			nextDirection = down;
+			Debug.Log ("down");
 		} else if (Input.GetKey(KeyCode.RightArrow)) {
-			currentDirection = right;
+			nextDirection = right;
+			Debug.Log ("right");
 		} else if (Input.GetKey(KeyCode.LeftArrow)) {
-			currentDirection = left;
-		} else {
-			isMoving = false;
+			nextDirection = left;
+			Debug.Log ("left");
 		}
 
-		transform.localEulerAngles = currentDirection;
+		if (AtCheckPoint ()) {
+			UpdateDirection ();
+			transform.localEulerAngles = currentDirection;
+			if ( !CanMove (GetBoardPosition(RoundVector3(transform.position + transform.forward * movementOffset,2)))) {
+				isMoving = false;
+			}
+		}
+
+		//transform.localEulerAngles = currentDirection;
 		animator.SetBool ("IsMoving", isMoving);
 
 		if (isMoving) {
-			transform.Translate(Vector3.forward * MovementSpeed * Time.deltaTime);
+			transform.Translate(RoundVector3(Vector3.forward *(movementOffset/ MovementSpeed), 2));
 		}
+	}
+
+	private Vector3 RoundVector3 ( Vector3 v, int decimals) {
+		return new Vector3 ((float)System.Math.Round (v.x, decimals), (float)System.Math.Round (v.y, decimals), (float)System.Math.Round (v.z, decimals));
 	}
 
 	void OnTriggerEnter(Collider col) {
