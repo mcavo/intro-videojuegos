51bbb8f3e5e70466293a15b90ef46ff33e04c295
Victoria <mvictoria.cavo@gmail.com>
2016-10-15 03:17:51 -0300

    Add some assets and camera controller

---
+++ tp3/CandyLand Adventure/Assets/Scripts/FollowMouseScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouseScript : MonoBehaviour {
+
+	private Vector3 RotationVector = new Vector3(0,1f,0);
+	public float RotationSpeed = 50f;
+	public float MovementSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	void Update()
+	{
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.localEulerAngles += RotationVector * RotationSpeed * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.localEulerAngles -= RotationVector * RotationSpeed * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.localPosition = transform.position + transform.forward * MovementSpeed * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.localPosition = transform.position - transform.forward * MovementSpeed * Time.deltaTime;
+		}
+	}
+}
