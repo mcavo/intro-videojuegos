4280f405e83ab30f00ff5450b3d16f264528b3c8
mariaeugenia <me.sakuda@hotmail.com>
2016-11-09 20:25:50 -0300

    Add sound manager for effects

--- tp3/CandyLand Adventure/Assets/Scripts/Dungeon/Bonus&Traps/Bonus.cs
+++ tp3/CandyLand Adventure/Assets/Scripts/Dungeon/Bonus&Traps/Bonus.cs
@@ -6,9 +6,14 @@ public class Bonus : MonoBehaviour {
 	public float incrementSpeed = 5f;
 	public float incrementTime = 1f;
 	public float duration = 1f;
+	public AudioClip audio;
 
 	void OnTriggerEnter(Collider col) {
 		GameManager.instance.TimeLeft -= incrementTime;
+		if (audio != null) 
+		{
+			SoundManager.instance.PlaySingle (audio);
+		}
 		gameObject.SetActive (false);
 	}
 }
